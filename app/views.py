0b87908fe2039b92ee18aa8934d5e4df4c6edbde
pugaru <30847672+pugaru@users.noreply.github.com>
2017-11-06 20:12:22 -0200

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+#from .forms import ContatoForm
+from core.models import Curso
+from core.forms import ContactForm
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    contexto={
+        "usuario":"Pessoa",
+        #aluno,professor,sbruble
+        "perfil":"aluno",
+        "cursos":Curso.objects.all()
+    }
+    return render(request,"index.html",contexto)
 
 def disciplina(request):
     return render(request,"disciplina.html")
@@ -15,3 +24,17 @@ def lista_cursos(request):
 
 def noticias(request):
     return render(request,"noticias.html")
+
+def contato(request):
+    if request.POST:
+        form = ContactForm(request.POST)
+        #form.enviar_email()
+    else:
+        form = ContactForm(request.POST)
+    contexto = {
+        "form":form
+    }
+    return render(request,"contato.html",contexto)
+
+def login(request):
+    return render(request,"login.html")
